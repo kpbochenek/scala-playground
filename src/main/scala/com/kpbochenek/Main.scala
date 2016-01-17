5e39d21d81c24698863cec87a2f9670c0d60edef
Krzysztof Bochenek <k.bochenek@ocado.com>
2016-01-17 10:46:23 +0100

    middle

--- src/main/scala/com/kpbochenek/Main.scala
+++ src/main/scala/com/kpbochenek/Main.scala
@@ -1,7 +1,24 @@
 package com.kpbochenek
 
+import scalaz.stream.Process
+
+
 object Main {
-    def main(args: Array[String]) {
-        println("MAIN")
-    }
+  def main(args: Array[String]): Unit = {
+    println("MAIN")
+
+    runTest()
+  }
+
+  def process(p: Process[Nothing, Int]) {
+    val result = p.map(_ + 10).filter(_ % 2 == 0).map(_.toString())
+    println(result)
+    println(result.toSource.runLog.run)
+
+  }
+
+  private def runTest() {
+    val p = Process(1, 2, 3, 4, 5)
+    process(p)
+  }
 }
