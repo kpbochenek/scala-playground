b5ee18eed74e9fe08dda1784a9285f6497f08c8f
kpbochenek <kpbochenek@gmail.com>
2017-03-27 01:17:48 +0200

    playground Fenwick Tree

--- FreeMonad/src/main/scala/com/kpbochenek/cats/FenwickTreeModel.scala
+++ FreeMonad/src/main/scala/com/kpbochenek/cats/FenwickTreeModel.scala
@@ -5,6 +5,7 @@ import cats.free.{Free, Inject}
 import cats.{Id, ~>}
 import com.kpbochenek.cats.FenwickPureInterpreter.FenwickTreeState
 import com.kpbochenek.cats.FenwickTreeModel.FenwickTreeModel
+import com.kpbochenek.cats.FenwickWrapper.{FUtils, FenwickUtils}
 
 import scala.com.kpbochenek.cats.FenwickTree
 
@@ -135,6 +136,61 @@ object UserInterpreterPure extends (UserInteractions.UserInteraction ~> FenwickT
   }
 }
 
+class UserInterpreterPureGenericAbs[G] {
+  type StateX[A] = State[G, A]
+
+  class UserInterpreterPureGeneric[A] extends (UserInteractions.UserInteraction ~> StateX) {
+    import UserInteractions._
+
+    override def apply[A](fa: UserInteraction[A]): State[G, A] = fa match {
+      case AskNumber(text) =>
+        println(text)
+        val v = Console.readInt()
+        println("Received: " + v)
+        State.pure(v.asInstanceOf[A])
+      case DisplayUser(text, value) =>
+        println("DISPLAY !@!@ " + text + value)
+        State.pure(().asInstanceOf[A])
+    }
+  }
+
+  def getInterpreter[A]: UserInterpreterPureGeneric[A] = new UserInterpreterPureGeneric[A]()
+}
+
+
+object FenwickWrapper {
+  sealed trait FenwickUtils[A]
+  case class AddList(x: List[Int]) extends FenwickUtils[Unit]
+  case class DisplayTree() extends FenwickUtils[Unit]
+  case class PartialSums(pos: Int) extends FenwickUtils[List[Int]]
+
+  class FUtils[F[_]](implicit I: Inject[FenwickUtils, F]) {
+
+    def addList(x: List[Int]): Free[F, Unit] =
+      Free.inject[FenwickUtils, F](AddList(x))
+    def displayTree(): Free[F, Unit] =
+      Free.inject[FenwickUtils, F](DisplayTree())
+    def partialSums(pos: Int): Free[F, List[Int]] =
+      Free.inject[FenwickUtils, F](PartialSums(pos))
+  }
+
+  object FUtils {
+    implicit def interacts[F[_]](implicit I: Inject[FenwickUtils, F]): FUtils[F] = new FUtils[F]
+  }
+}
+
+object FenwickWrapperInterpreter extends (FenwickUtils ~> FenwickTreeState) {
+  import FenwickWrapper._
+
+  override def apply[A](fa: FenwickUtils[A]): FenwickTreeState[A] = fa match {
+    case AddList(x) =>
+      State.modify(s => {x.foreach(i => s.add(i)); s})
+    case DisplayTree() =>
+      State.pure(().asInstanceOf[A])
+    case PartialSums(pos) =>
+      State.inspect(s => (1 to pos).map(i => s.sum(i)).toList.asInstanceOf[A])
+  }
+}
 
 object FenwickExample {
   import FenwickTreeModel._
@@ -142,6 +198,7 @@ object FenwickExample {
 
   type FullApp[A] = Coproduct[FenwickTreeModel, UserInteraction, A]
 
+  type UberApp[A] = Coproduct[FenwickUtils, FullApp, A]
 
   def program(implicit I : Interacts[FullApp],
               D : FenwickActions[FullApp]): Free[FullApp, Int] = {
@@ -186,6 +243,24 @@ object FenwickExample {
     } yield result
   }
 
+  def program4(implicit I : Interacts[UberApp],
+               D : FenwickActions[UberApp],
+              U : FUtils[UberApp]): Free[UberApp, List[Int]] = {
+
+    import I._, D._, U._
+
+    for {
+      a <- askNumber("N1:")
+      b <- askNumber("N2:")
+      c <- askNumber("N3:")
+      _ <- put(a+5, 1)
+      _ <- put(b+5, 1)
+      _ <- put(c+5, 1)
+      _ <- addList(List(a, b, c))
+      ps <- partialSums(10)
+    } yield ps
+  }
+
   def main(args: Array[String]): Unit = {
     println("RUN")
 
@@ -204,15 +279,31 @@ object FenwickExample {
     // asking you to provide 2 numbers
 //    program2.foldMap(interpreter)
 
+    // run with state from previous tests
 //    program3.foldMap(interpreterImpure)
 
+    // explicitly provide starting state
+//    val interpreterPure: FullApp ~> FenwickTreeState = FenwickPureInterpreter.pureInterpreter or UserInterpreterPure
+//    val result = program3.foldMap(interpreterPure).run(new FenwickTree(20)).value
+//
+//    println("RESULT!! ")
+//    println(result)
+
+    // more generic interpreter accepting any type of state
+    val userInterpreterAbs = new UserInterpreterPureGenericAbs[FenwickTree]
     val interpreterPure: FullApp ~> FenwickTreeState =
-      FenwickPureInterpreter.pureInterpreter or UserInterpreterPure
+      FenwickPureInterpreter.pureInterpreter or userInterpreterAbs.getInterpreter
 
-    val result = program3.foldMap(interpreterPure).run(new FenwickTree(20)).value
+//    val result = program3.foldMap(interpreterPure).run(new FenwickTree(20)).value
 
-    println("RESULT!! ")
-    println(result)
+//    println("RESULT!! ")
+//    println(result)
+
+        val interpreter3Phase: UberApp ~> FenwickTreeState = FenwickWrapperInterpreter or interpreterPure
+        val result = program4.foldMap(interpreter3Phase).run(new FenwickTree(20)).value
+
+        println("RESULT!! ")
+        println(result)
   }
 }
 
